19254206bf280ea3d0d7ef27f11db413cbad53df
Brandom Bermudez <brandomakcode@outlook.com>
2024-08-02 18:06:15 -0400

    Model renamed, renamed based in best practices

--- real_estate/models/real_estate_model.py
+++ real_estate/models/real_estate_model.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 class EstateProperty(models.Model):
-    _name = "real_estate.property"
+    _name = "real_estate.estate_property"
     _description = "Test Model"
 
     name = fields.Char()
