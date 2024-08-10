d6a26ebb2e8285a20111c4f83821f3bbdf197dd6
brandomAKcode <brandomhk@gmail.com>
2024-08-09 20:25:57 -0400

    Fields of models was modified, the computed and change decorator was added

--- real_estate/models/real_estate_model.py
+++ real_estate/models/real_estate_model.py
@@ -1,6 +1,7 @@
 from email.policy import default
 from importlib.metadata import requires
-from odoo import fields, models
+from odoo import fields, models, api
+from datetime import datetime, timedelta
 
 
 class EstateProperty(models.Model):
@@ -14,11 +15,11 @@ class EstateProperty(models.Model):
     expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
-    living_area = fields.Integer()
+    living_area = fields.Integer(default=0)
     facades = fields.Integer()
     garage = fields.Integer(default=False)
     garden = fields.Boolean(default=False)
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(default=0)
     garden_orientation = fields.Selection([
         ('north', 'North'),
         ('south', 'South'),
@@ -31,6 +32,24 @@ class EstateProperty(models.Model):
     salesman_id = fields.Many2one(comodel_name='res.users', ondelete='set null', default=lambda self: self.env.user)
     tag_ids = fields.Many2many(comodel_name='real_estate.property.tag', ondelete="restrict")
     offer_ids = fields.One2many(comodel_name='real_estate.property.offer', inverse_name="property_id")
+    # computed field
+    total_area = fields.Float(compute="_compute_total_area")
+    best_offer = fields.Float(compute="_compute_best_offer")
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_offer(self):
+        for record in self:
+            record.best_offer = max(record.offer_ids.mapped('price') if len(record.offer_ids) > 0 else [0])
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
 
 
 class EstatePropertyType(models.Model):
@@ -56,5 +75,18 @@ class EstatePropertyOffer(models.Model):
         ('accepted', 'Accepted'),
         ('refused', 'Refused')
     ], string='Offer Status', default='refused')
+    validity = fields.Integer(default=7)
+    date_created = fields.Date(default=datetime.now().date(), readonly=True)
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline', readonly=False)
     buyer_id = fields.Many2one(comodel_name='res.partner', ondelete='cascade')
+
     property_id = fields.Many2one(comodel_name='real_estate.property', ondelete='cascade')
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = record.date_created + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.date_created).days
