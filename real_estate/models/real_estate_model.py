e6689200df530d75f159aaf6bd07ca53f4005ebf
brandomAKcode <brandomhk@gmail.com>
2024-08-11 20:35:40 -0400

    Models updated and new functions was added

--- real_estate/models/real_estate_model.py
+++ real_estate/models/real_estate_model.py
@@ -1,7 +1,6 @@
-from email.policy import default
-from importlib.metadata import requires
 from odoo import fields, models, api
 from datetime import datetime, timedelta
+from odoo.exceptions import UserError
 
 
 class EstateProperty(models.Model):
@@ -26,7 +25,12 @@ class EstateProperty(models.Model):
         ('east', 'East'),
         ('west', 'West')
     ], string='Garden Orientation', default='north')
-    # Many2one field
+    status = fields.Selection([
+        ('available', 'Available'),
+        ('canceled', 'Canceled'),
+        ('sold', 'Sold')
+    ], string='Status of property', default='available')
+    # relations
     type_id = fields.Many2one(comodel_name='real_estate.property.type', ondelete='set null')
     buyer_id = fields.Many2one(comodel_name='res.partner', ondelete='set null')
     salesman_id = fields.Many2one(comodel_name='res.users', ondelete='set null', default=lambda self: self.env.user)
@@ -51,6 +55,20 @@ class EstateProperty(models.Model):
         if self.garden:
             self.garden_area = 10
 
+    def action_sold(self):
+        if self.status == 'canceled':
+            raise UserError("The property cannot be sold because it has been canceled.")
+
+        self.status = 'sold'
+        return True
+
+    def action_canceled(self):
+        if self.status == 'sold':
+            raise UserError("The property cannot be canceled as it has already been sold.")
+
+        self.status = 'canceled'
+        return True
+
 
 class EstatePropertyType(models.Model):
     _name = 'real_estate.property.type'
@@ -72,14 +90,14 @@ class EstatePropertyOffer(models.Model):
 
     price = fields.Float()
     status = fields.Selection([
+        ('revision', 'Revision'),
         ('accepted', 'Accepted'),
         ('refused', 'Refused')
-    ], string='Offer Status', default='refused')
+    ], string='Offer Status', default='revision')
     validity = fields.Integer(default=7)
     date_created = fields.Date(default=datetime.now().date(), readonly=True)
     date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline', readonly=False)
     buyer_id = fields.Many2one(comodel_name='res.partner', ondelete='cascade')
-
     property_id = fields.Many2one(comodel_name='real_estate.property', ondelete='cascade')
 
     @api.depends('validity')
@@ -90,3 +108,26 @@ class EstatePropertyOffer(models.Model):
     def _inverse_date_deadline(self):
         for record in self:
             record.validity = (record.date_deadline - record.date_created).days
+
+    def action_accept_offer(self):
+        if self.status == 'accepted' or self.status == 'refused':
+            raise UserError('The offer status cannot be changed, as it may have already been accepted or refused')
+        elif self.property_id.status == 'canceled' or self.property_id.status == 'sold':
+            raise UserError('The offer status cannot be changed, as it may have already been sold or canceled')
+
+        self.status = 'accepted'
+        self.property_id.status = 'sold'
+        self.property_id.buyer_id = self.buyer_id
+        self.property_id.selling_price = self.price
+
+        return True
+
+    def action_refuse_offer(self):
+        if self.status == 'accepted' or self.status == 'refused':
+            raise UserError('The offer status cannot be changed, as it may have already been accepted or refused')
+        elif self.property_id.status == 'canceled' or self.property_id.status == 'sold':
+            raise UserError('The offer status cannot be changed, as it may have already been sold or canceled')
+
+        self.status = 'refused'
+
+        return True
